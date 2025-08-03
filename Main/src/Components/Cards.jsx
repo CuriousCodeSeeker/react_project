e860d6749dee292b617066bc235ac68e357e5eb6
Warjot kaur <curiouscodeseeker@gmail.com>
2025-08-03 19:13:15 +0530

    details added

--- Main/src/Components/Cards.jsx
+++ Main/src/Components/Cards.jsx
@@ -2,9 +2,10 @@ import books from '../assets/books.json';
 import Card from './Card';
 
 const Cards = () => {
+    const renderBooks = books.slice(0,3)
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-4">
-            {books.map((book) => (
+            {renderBooks.map((book) => (
                 <Card key={book.id} {...book} />
             ))}
         </div>
